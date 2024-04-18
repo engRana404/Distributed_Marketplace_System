8815659aefd3f0d61bed3793c3c10d00ced8ae8b
HazemHamdyy <hazemhamdy389@gmail.com>
2024-04-19 00:20:30 +0200

    change port

--- src/app.js
+++ src/app.js
@@ -36,7 +36,7 @@ app.use(errorHandler)
 
  
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
